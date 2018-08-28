70f7c5bbb8e57e7638c4d335623230477f376b7a
Mark Houston <markhouston35@gmail.com>
2018-08-27 21:51:30 -0400

    Implement basic system services

--- mhktx/src/main/java/com/markhouston/mhktx/ContextExt.kt
+++ mhktx/src/main/java/com/markhouston/mhktx/ContextExt.kt
@@ -1,14 +1,37 @@
 package com.markhouston.mhktx
 
+import android.app.NotificationManager
 import android.content.Context
+import android.net.ConnectivityManager
+import android.os.PowerManager
+import android.os.UserManager
 import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
 import android.util.DisplayMetrics
+import android.view.WindowManager
 
 /**
  * Created by Mark.Houston on 8/9/2018
  */
+
+inline val Context.windowManger
+    get() = getSystemService(Context.WINDOW_SERVICE) as WindowManager?
+
+inline val Context.powerService
+    get() = getSystemService(Context.POWER_SERVICE) as PowerManager?
+
+inline val Context.notificationManger
+    get() = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+inline val Context.connectivityManager
+    get() = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+
+inline val Context.userManager
+    get() = getSystemService(Context.USER_SERVICE) as UserManager?
+
+inline val Context.displayMetrics: DisplayMetrics
+    get() = resources.displayMetrics
 
 /**
  * Retrieve a color resource.
@@ -25,19 +48,13 @@ fun Context.getColorCompat(@ColorRes colorRes: Int) = ContextCompat.getColor(thi
 fun Context.getDrawableCompat(@DrawableRes drawableRes: Int) = ContextCompat.getDrawable(this, drawableRes)
 
 /**
- * Retrieve the display metrics of the device.
- * @return The display metrics of the device.
- */
-fun Context.displayMetrics(): DisplayMetrics = resources.displayMetrics
-
-/**
  * Retrieve the width pixels from the display metrics of the device.
  * @return The width pixels from the display metrics of the device.
  */
-fun Context.displayWidth(): Int = displayMetrics().widthPixels
+fun Context.displayWidth(): Int = displayMetrics.widthPixels
 
 /**
  * Retrieve the height pixels from the display metrics of the device.
  * @return The height pixels from the display metrics of the device.
  */
-fun Context.displayHeight(): Int = displayMetrics().heightPixels
+fun Context.displayHeight(): Int = displayMetrics.heightPixels
